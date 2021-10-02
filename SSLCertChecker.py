438d4cf524847ffc92f79350689628c11564e41f
Sergei Fedorov <8ergo@mail.ru>
2021-10-02 17:45:31 +0300

    Fix code style

--- SSLCertChecker.py
+++ SSLCertChecker.py
@@ -5,83 +5,91 @@ __maintainer__ = "Fedorov Sergei"
 __email__ = "8ergo@mail.ru"
 __status__ = "Production"
 
+import datetime
 from requests.packages.urllib3.contrib import pyopenssl as reqs
 import requests
-import datetime
 
-debug = False # Отвечает за вывод инфы для диагностики в консоль
-enable_alerts_to_tg = True # Отвечает за отправку уведомлений в TG
-bot_token = '' # Token бота
-bot_chatID = '' # ID чата в который отправлять уведомления
-path_to_host_file = './host_list.txt' # Путь к файлу списка доменных имен для проверки сертификатов
-days_before_alert = 30 # За сколько дней начать уведомлять об окончании сертификата
+
+DEBUG = False # Отвечает за вывод инфы для диагностики в консоль
+ENABLE_ALERTS_TO_TG = True # Отвечает за отправку уведомлений в TG
+BOT_TOKEN = '' # Token бота
+BOT_CHAT_ID = '' # ID чата в который отправлять уведомления
+PATH_TO_HOST_FILE = './host_list.txt' # Путь к файлу списка доменных имен для проверки сертификатов
+DAYS_BEFORE_ALERT = 30 # За сколько дней начать уведомлять об окончании сертификата
 
 
 def alert_to_tg(msg_text):
     '''Send message in TG'''
-    response = requests.post(
-        url=f"https://api.telegram.org/bot{bot_token}/sendMessage",
-        data={'chat_id': bot_chatID, 'text': msg_text}
+    tg_result = requests.post(
+        url=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={'chat_id': BOT_CHAT_ID, 'text': msg_text}
     )
-    return response.json()
+    return tg_result.json()
 
 
-def check_cert(cert, host):
+def check_cert(cert, host_for_check):
     '''Check date in cert and now'''
+    check_result = None
     current_date = datetime.datetime.now()
-    expiration_date = datetime.datetime.strptime(cert.get_notAfter().decode("utf-8"), '%Y%m%d%H%M%SZ')
-    begin_date = datetime.datetime.strptime(cert.get_notBefore().decode("utf-8"), '%Y%m%d%H%M%SZ')
+    expiration_date = datetime.datetime.strptime(
+        cert.get_notAfter().decode("utf-8"),
+        '%Y%m%d%H%M%SZ'
+        )
+    begin_date = datetime.datetime.strptime(
+        cert.get_notBefore().decode("utf-8"),
+        '%Y%m%d%H%M%SZ'
+        )
     betwin = expiration_date - current_date
-    
-    if debug:
+    if DEBUG:
         print(f"Date begin: {begin_date}")
         print(f"Date end: {expiration_date}")
         print(f"Days before end: {betwin}")
-
     if current_date < begin_date:
-        return f"‼️ Внимание ‼️\n⚠️Для {host} срок действия SSL-сертификата еще не начался.\n📅Начало {str(begin_date)}."
-    elif betwin.days <= days_before_alert:
-        return f"‼️ Внимание ‼️\n⚠️Для {host} скоро закончится срок действия SSL-сертификата.\n📅Осталось {str(betwin.days)} дней."
+        check_result = f"‼️ Внимание ‼️\n⚠️Для {host_for_check} срок действия SSL-сертификата еще не начался.\n📅Начало {str(begin_date)}."
+    elif betwin.days <= DAYS_BEFORE_ALERT:
+        check_result = f"‼️ Внимание ‼️\n⚠️Для {host_for_check} скоро закончится срок действия SSL-сертификата.\n📅Осталось {str(betwin.days)} дней."
     elif current_date >= expiration_date:
-        return f"‼️ Внимание ‼️\n⚠️Для {host} {str(expiration_date)} закончился срок действия SSL-сертификата."
+        check_result = f"‼️ Внимание ‼️\n⚠️Для {host_for_check} {str(expiration_date)} закончился срок действия SSL-сертификата."
     else:
-        return "ok"
+        check_result = "ok"
+    return check_result
 
 
-def get_cert(host, port=443):
+def get_cert(host_for_check, port_for_check=443):
     '''Get cert for host'''
     try:
-        cert = reqs.OpenSSL.crypto.load_certificate(reqs.OpenSSL.crypto.FILETYPE_PEM, reqs.ssl.get_server_certificate((host, port)))
+        cert = reqs.OpenSSL.crypto.load_certificate(
+            reqs.OpenSSL.crypto.FILETYPE_PEM,
+            reqs.ssl.get_server_certificate((host_for_check, port_for_check))
+            )
         return cert
     except Exception as ex:
         print(ex)
         return None
 
 
-with open(path_to_host_file, 'r', encoding='UTF-8') as host_file:
+with open(PATH_TO_HOST_FILE, 'r', encoding='UTF-8') as host_file:
     for line in host_file.readlines():
         print('='*10)
         host_cert = None
         result = None
         host_port = line.rstrip('\n').split(':')
         host = host_port[0]
-
         if len(host_port) > 1:
             port = host_port[1]
         else:
             port = 443
-       
         print(f"{host}:{port}")
         host_cert = get_cert(host, port)
-        if host_cert == None:
+        if host_cert is None:
             continue
         result = check_cert(host_cert, host)
         print(result)
-        if result != 'ok' and enable_alerts_to_tg:
+        if result != 'ok' and ENABLE_ALERTS_TO_TG:
             response = alert_to_tg(result)
-            if debug:
+            if DEBUG:
                 print(response)
-            if response['ok']==True:
+            if response['ok']:
                 print('Уведомление отправлено в ТГ')
             else:
                 print('Проблема с отправкой уведомления')
